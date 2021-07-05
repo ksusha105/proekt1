a4006294f585af87fbdf600da344fefea727203c
ksusha105 <kdima874@icloud.com>
2021-07-05 15:21:44 +0300

    init commit

--- js.js
+++ js.js
@@ -2,12 +2,14 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=Mogilev&lang=ru&appid=d1
     .then(function(resp) { return resp.json()})
     .then(function(data){
         console.log(data);
-        document.querySelector('.package-name').textContent=data.name;
-        document.querySelector('.price').innerHTML='+'+Math.round(data.main.temp - 273) + '&deg;';
-        document.querySelector('.price2').innerHTML=Math.round(data.main.feels_like - 273) + '&deg;';
-        document.querySelector('.disclaimer').textContent=data.weather[0].description;
-        document.querySelector('.features ').innerHTML='<img src="http://openweathermap.org/img/wn/03d@2x.png">'
-        document.querySelector('.wind').textContent=data.wind.speed+' '+'м/с';
+        document.querySelector('.city').textContent=data.name;
+        document.querySelector('.temp').innerHTML='+'+Math.round(data.main.temp - 273) + '&deg;';
+        document.querySelector('.temp2').innerHTML='Ощущается как'+' '+'+'+Math.round(data.main.feels_like - 273) + '&deg;';
+        document.querySelector('.pressure').innerHTML='Давление'+' '+Math.round(data.main.pressure / 1.333) + ' ' + 'мм рт.ст.';
+        document.querySelector('.humidity').innerHTML='Влажность'+' '+Math.round(data.main.humidity) + ' ' + '%';
+        document.querySelector('.picture ').innerHTML=`<img src="http://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
+        document.querySelector('.weather').textContent=data.weather[0].description;
+        document.querySelector('.wind').textContent='Ветер'+' '+data.wind.speed+' '+'м/с';
     })
     .catch(function(){
 
